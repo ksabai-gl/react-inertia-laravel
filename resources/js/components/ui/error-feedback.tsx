5170ee72a6c8d18131b175222390997cd3b11c77
vineeth-gl <vineeth.kodakadan@globallogic.com>
2026-07-20 18:20:04 +0530

    fix error feedback class merging

--- resources/js/components/ui/error-feedback.tsx
+++ resources/js/components/ui/error-feedback.tsx
@@ -6,7 +6,7 @@ export default function ErrorFeedback({
     ...props
 }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
     return message ? (
-        <p {...props} className={'text-destructive text-sm' + className}>
+        <p {...props} className={`text-destructive text-sm ${className}`.trim()}>
             {message}
         </p>
     ) : null;
